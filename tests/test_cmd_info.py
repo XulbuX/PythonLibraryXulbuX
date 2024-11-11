69bbecfe8fa3822ed73c090e71d6b9fa85397b8e
XulbuX <142539516+XulbuX-dev@users.noreply.github.com>
2024-11-11 23:36:43 +0100

    x

--- tests/test_cmd_info.py
+++ tests/test_cmd_info.py
@@ -1,22 +1,19 @@
 import src.XulbuX as xx
 
 
-def test_user_not_empty():
+def test_console_user():
     user_output = xx.Cmd.user()
-    assert user_output != ""  # Check that user() does not return an empty string
+    assert isinstance(user_output, str)
+    assert user_output != ""
 
-def test_width_not_zero():
+def test_console_width():
     width_output = xx.Cmd.w()
-    assert width_output != 0  # Check that w() does not return zero
+    assert isinstance(width_output, int)
+    assert width_output != 0
+    assert width_output >= 0
 
-def test_height_not_zero():
+def test_console_height():
     height_output = xx.Cmd.h()
-    assert height_output != 0  # Check that h() does not return zero
-
-def test_width_not_negative():
-    width_output = xx.Cmd.w()
-    assert width_output >= 0  # Check that w() does not return a negative value
-
-def test_height_not_negative():
-    height_output = xx.Cmd.h()
-    assert height_output >= 0  # Check that h() does not return a negative value
+    assert isinstance(height_output, int)
+    assert height_output != 0
+    assert height_output >= 0
