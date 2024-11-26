51ad5f1cd0e2cd29fe5e676ea91827120e7d3d72
XulbuX <142539516+XulbuX-dev@users.noreply.github.com>
2024-11-26 09:25:40 +0100

    x

--- tests/test_format_codes.py
+++ tests/test_format_codes.py
@@ -3,18 +3,18 @@ from xulbux import FormatCodes
 
 
 black = ANSI.seq_color.format(0, 0, 0)
-bg_bright_red = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[("bg:bright:red", "bg:br:red")]}{ANSI.end}"
+bg_bright_red = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[('bg:bright:red', 'bg:br:red')]}{ANSI.end}"
 orange = ANSI.seq_color.format(255, 136, 119)
 
-bold = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[("bold", "b")]}{ANSI.end}"
-italic = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[("italic", "i")]}{ANSI.end}"
-underline = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[("underline", "u")]}{ANSI.end}"
+bold = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[('bold', 'b')]}{ANSI.end}"
+italic = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[('italic', 'i')]}{ANSI.end}"
+underline = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[('underline', 'u')]}{ANSI.end}"
 
-reset_bg = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[("_background", "_bg")]}{ANSI.end}"
-reset_bold = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[("_bold", "_b")]}{ANSI.end}"
-reset_color = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[("_color", "_c")]}{ANSI.end}"
-reset_italic = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[("_italic", "_i")]}{ANSI.end}"
-reset_underline = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[("_underline", "_u")]}{ANSI.end}"
+reset_bg = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[('_background', '_bg')]}{ANSI.end}"
+reset_bold = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[('_bold', '_b')]}{ANSI.end}"
+reset_color = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[('_color', '_c')]}{ANSI.end}"
+reset_italic = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[('_italic', '_i')]}{ANSI.end}"
+reset_underline = f"{ANSI.char}{ANSI.start}{ANSI.codes_map[('_underline', '_u')]}{ANSI.end}"
 
 
 def test_codes_to_ansi():
