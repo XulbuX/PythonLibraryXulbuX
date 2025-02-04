11ba610fcab82e3817dd4585fc0b3ad559b0a5b6
XulbuX <142539516+XulbuX-dev@users.noreply.github.com>
2025-02-04 14:25:18 +0100

    x

--- src/xulbux/xx_data.py
+++ src/xulbux/xx_data.py
@@ -482,15 +482,15 @@ class Data:
             else:
                 return (
                     (
-                        f"{punct['"']}[{_syntax_hl['str']}]{String.escape(str(value), '"')}[_]{punct['"']}"
+                        f"{punct['\"']}[{_syntax_hl['str']}]{String.escape(str(value), '\"')}[_]{punct['\"']}"
                         if syntax_hl
-                        else f"{punct['"']}{String.escape(str(value), '"')}{punct['"']}"
+                        else f"{punct['\"']}{String.escape(str(value), '\"')}{punct['\"']}"
                     )
                     if as_json
                     else (
-                        f'{punct["'"]}[{_syntax_hl["str"]}]{String.escape(str(value), "'")}[_]{punct["'"]}'
+                        f'{punct["\'"]}[{_syntax_hl["str"]}]{String.escape(str(value), "\'")}[_]{punct["\'"]}'
                         if syntax_hl
-                        else f'{punct["'"]}{String.escape(str(value), "'")}{punct["'"]}'
+                        else f'{punct["\'"]}{String.escape(str(value), "\'")}{punct["\'"]}'
                     )
                 )
 
