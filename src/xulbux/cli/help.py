d26adb3b7a51eabd1399c4636780bd487de674d6
XulbuX <j.buytenhuijs@gmail.com>
2025-09-03 14:04:36 +0200

    update `1.8.1`

--- src/xulbux/cli/help.py
+++ src/xulbux/cli/help.py
@@ -3,22 +3,50 @@ from ..base.consts import COLOR
 from ..format_codes import FormatCodes
 from ..console import Console
 
+from urllib.error import HTTPError
+from typing import Optional
+import urllib.request as _request
+import json as _json
 
+
+def get_latest_version() -> Optional[str]:
+    with _request.urlopen(URL) as response:
+        if response.status == 200:
+            data = _json.load(response)
+            return data["info"]["version"]
+        else:
+            raise HTTPError(URL, response.status, "Failed to fetch latest version info", response.headers, None)
+
+
+def is_latest_version() -> Optional[bool]:
+    try:
+        latest = get_latest_version()
+        if latest in ("", None): return None
+        latest_v_parts = tuple(int(part) for part in latest.lower().lstrip("v").split('.'))
+        installed_v_parts = tuple(int(part) for part in __version__.lower().lstrip("v").split('.'))
+        return latest_v_parts <= installed_v_parts
+    except Exception:
+        return None
+
+
+URL = "https://pypi.org/pypi/xulbux/json"
+IS_LATEST_VERSION = is_latest_version()
 CLR = {
     "class": COLOR.TANGERINE,
+    "code_border": COLOR.GRAY,
     "const": COLOR.RED,
     "func": COLOR.CYAN,
     "import": COLOR.NEON_GREEN,
     "lib": COLOR.ORANGE,
+    "notice": COLOR.YELLOW,
     "punctuators": COLOR.DARK_GRAY,
-    "code_border": COLOR.GRAY,
 }
 HELP = FormatCodes.to_ansi(
     rf"""  [_|b|#7075FF]               __  __
   [b|#7075FF]  _  __ __  __/ / / /_  __  ___  __
   [b|#7075FF] | |/ // / / / / / __ \/ / / | |/ /
   [b|#7075FF] > , </ /_/ / /_/ /_/ / /_/ /> , <
-  [b|#7075FF]/_/|_|\____/\__/\____/\____//_/|_|  [*|BG:{COLOR.GRAY}|#000] v[b]{__version__} [*]
+  [b|#7075FF]/_/|_|\____/\__/\____/\____//_/|_|  [*|BG:{COLOR.GRAY}|#000] v[b]{__version__} [*|dim|{CLR['notice']}]({'' if IS_LATEST_VERSION else ' (newer available)'})[*]
 
   [i|{COLOR.CORAL}]A TON OF COOL FUNCTIONS, YOU NEED![*]
 
